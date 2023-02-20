c9260dd7108e52adc424af49273b1a56a0066da3
Olaf-Tarkowski <tarkowski.software@gmail.com>
2023-02-20 02:09:41 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import CardSlider from "./components/Home/CardSlider";
+import NavBar from "./components/Home/NavBar";
+import Header from "./components/Layout/Header";
+
 function App() {
-  return <>hi</>;
+  return (
+    <>
+      <Header />
+      <NavBar />
+      <CardSlider />
+    </>
+  );
 }
 
 export default App;
